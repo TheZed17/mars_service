5106b2e8d6ecabcc2f47c1f4e2077d042da34ba4
Igor Deminski <kartoschka17gmail.com>
2023-04-29 15:02:04 +0300

    [update] start orders_app, admin, application model

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MARS_SERVICE PROJECT
+
+python manage.py startapp orders_app
---
+++ mars_service/orders_app/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+def status_validator(order_status):
+    return order_status in ["open", "closed", "in progress", "need info"]
+
+
+class Order(models.Model):
+    order_description = models.TextField(verbose_name="Описание")
+    created_dt = models.DateTimeField(verbose_name="Создано", auto_created=True)
+    updated_dt = models.DateTimeField(verbose_name="Изменено", blank=True, null=True)
+    order_status = models.TextField(validators=[status_validator])
