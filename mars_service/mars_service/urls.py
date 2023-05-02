c1835dfc462e145eb2da741d8578baa92734d81f
Igor Deminski <kartoschka17gmail.com>
2023-05-02 15:39:54 +0300

    [update] add html pages

--- mars_service/mars_service/urls.py
+++ mars_service/mars_service/urls.py
@@ -13,10 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+
+import orders_app.views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('orders_app/', include('orders_app.urls')),
-]
+    path('admin/', admin.site.urls),
+    path('', orders_app.views.home, name='home'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
