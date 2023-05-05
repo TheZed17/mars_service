5fa4db7723fc0cc1e0a92b164ca7fab75d32d1da
Igor Deminski <kartoschka17gmail.com>
2023-05-05 16:44:14 +0300

    [update] html pages, static

--- mars_service/mars_service/settings.py
+++ mars_service/mars_service/settings.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-import os
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-dw1$!se3^jvjb4%y74#)055aw_15m#f+%dh&c-o7(y^nyv2aae
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -73,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mars_service.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -87,7 +84,6 @@ DATABASES = {
         'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -107,7 +103,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -121,12 +116,17 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'mars_service/static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = os.path.join('/static/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- mars_service/orders_app/views.py
+++ mars_service/orders_app/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
 
 
-def home(request):
-    return render(request, "orders_app/home.html")
-
-
 def mainpage(request):
-    return render(request, "orders_app/mainpage.html")
+    data = {
+        "data": [
+            {
+                "button_link": "admin",
+                "name": "Заявки",
+                "overview": "Работа с заявками на оборудование",
+                "img_src": "{% static '1.png' %}"
+            },
+            {
+                "button_link": "#",
+                "name": "Персонал",
+                "overview": "Работа с базами персонала",
+                "img_src": "{% static '2.png' %}"
+            },
+            {
+                "button_link": "#",
+                "name": "Финансы",
+                "overview": "Работа с базами финансов",
+                "img_src": "{% static '3.png' %}"
+            },
+            {
+                "button_link": "#",
+                "name": "База знаний",
+                "overview": "Работа с базами знаний",
+                "img_src": "{% static '4.png' %}"
+            },
+        ]
+    }
+    return render(request, "orders_app/mainpage.html", data)
